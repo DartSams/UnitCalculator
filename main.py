f6eabbfdb46b960b2f02a8f0f34570c25cd32e6e
DartSams <dartagnansams1@icloud.com>
2022-07-01 14:27:32 -0400

    add rounding to the 5th decimal place,fixed css of sidebar,and removed js length file that is now being handled on the backend

--- main.py
+++ main.py
@@ -18,18 +18,17 @@ def home():
     return render_template("home.html")
 
 @socketio.on('original')
-def handle_message(socket_data:dict): #better than making 100 different functions for every conversion type
+def handle_message(socket_data:dict): #better than making 100 different functions for every conversion type also rounds to the 5th decimal place
     print(socket_data)
     converted_data = {}
-    # print(UNITS[socket_data["unit"]])
     for original_type in UNITS[socket_data["unit"]][socket_data["originalConversion"]].items(): #indexes the dictionary to return the unit values
         name = original_type[0] #returns the name of the unit currently being converted
         original_data = original_type[1] 
         if original_data["operation"] == "multiply":
-            converted_data[name] = float(socket_data["originalNumber"]) * original_data["conversion_num"]
+            converted_data[name] = round(float(socket_data["originalNumber"]) * original_data["conversion_num"],5)
         
         elif original_data["operation"] == "divide":
-            converted_data[name] = float(socket_data["originalNumber"]) / original_data["conversion_num"]
+            converted_data[name] = round(float(socket_data["originalNumber"]) / original_data["conversion_num"],5)
 
     # return converted_data
     emit('new_data',converted_data) #sends data to frontend
